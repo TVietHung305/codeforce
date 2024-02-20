7100539043b5c6ba94a9d37a682d450b7ae193b1
Harris Tran <120624208+TVietHung305@users.noreply.github.com>
2024-02-20 23:50:45 +0700

    Update beautiful_matrix.c

--- beautiful_matrix.c
+++ beautiful_matrix.c
@@ -1,18 +1,4 @@
 #include<stdio.h>
-# define MAX 5
-
-void find_location(int arr[MAX][MAX], int *row, int *col){
-	int k,l;
-	for( k = 0; k < 5; k++){
-		for( l = 0; l < 5; l++){
-			if(arr[k][l] == 1){
-				*row = k;
-				*col = l;
-				return;
-			}
-		}
-	}
-}
 
 int main(){
 	int arr[MAX][MAX];
@@ -22,31 +8,11 @@ int main(){
 	for(i = 0; i < 5; i++){
 		for(j = 0; j < 5; j++){
 			scanf("%d", &arr[i][j]);
+			if(arr[i][j] == 1){
+				count = abs(i-3) + abs(j-3);
+			}
 		}
-	}
-	find_location(arr, &row, &col);
-	
-	do{
-		if(arr[row][col] == arr[2][2] && arr[2][2] == 1){
-			break;
-		}
-		
-		if(row < 2){
-			row++;
-			count++;	
-		}else if(row > 2){
-			row--;
-			count++;
-		}
-		if(col < 2){
-			col++;
-			count++;	
-		}else if(col > 2){
-			col--;
-			count++;
-		}
-		
-	}while(row != 2 || col != 2);
+	}	
 	printf("%d\n", count);
 	return 0;
 }
